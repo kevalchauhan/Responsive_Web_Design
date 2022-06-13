18ec871aac7ea3a674dc43ccc891cc9931b7f473
kevalchauhan <vasuchauhan202020@gmail.com>
2022-06-13 11:47:55 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+
+
+# THE BRAND - A RESPONSIVE WEB DESIGN
+
+
+👉 Responsive E-Commerece Website Using HTML, CSS & JavaScript.  
+👉 Contains animations when scrolling.   
+👉 It has three color designs (blue ,light grey and black)  
+👉 Smooth scrolling in each section.  
+👉 Developed first with the Desktop First methodology, then for Mobile.  
+👉Compatible with all mobile devices.
+
+
+## Demo
+
+![1](https://user-images.githubusercontent.com/99174820/167103790-1e85bcf8-a3e2-41f3-8b7e-a6f63cb8f674.jpg)
+![2](https://user-images.githubusercontent.com/99174820/167103811-85ccb775-9b2e-4e36-a2ea-fb926de6e194.jpg)
+![3](https://user-images.githubusercontent.com/99174820/167103814-a0bb24a4-8f2f-4636-a4ec-eee756d1e045.jpg)
+
+## Tools & languages:
+
+HTML
+CSS
+JavaScript
+Visual Studio Code ( Text editor )
+
+## Author
+https://github.com/kevalchauhan
+
+## License
+No License Needed For This Project
+
+## Show your support
+Please ⭐️ this repository if this project helped you!
---
+++ script.js
@@ -0,0 +1,50 @@
+var preloader = document.getElementById('loading');
+    function myFunction(){
+        preloader.style.display = 'none';
+    }
+
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+
+menu.onclick= () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll= () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+document.querySelector('#search-icon').onclick = () => {
+    document.querySelector('#search-form').classList.toggle('active');
+}   
+
+document.querySelector('#close').onclick = () => {
+    document.querySelector('#search-form').classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+});
+
+// function loader(){
+//     document.querySelector('.loader-container').classList.add('fade-out');
+// }
+
+// function fadeOut(){
+//     setInterval(loader,1);
+// }
+
+// $(window).onload = fadeOut;
